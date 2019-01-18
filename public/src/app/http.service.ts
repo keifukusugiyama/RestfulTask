7c50934df1612da724598202e41512fd6d6285e9
Kay Sugiyama <keifuku.sugiyama@gmail.com>
2019-01-17 16:04:44 -0800

    fix the public folder

---
+++ public/src/app/http.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+  constructor(private _http: HttpClient) {
+    this.getTasks();
+    this.getTaskByID();
+  }
+
+  getTasks(){
+    // our http response is an Observable, store it in a variable
+    let tempObservable = this._http.get('/api/tasks');
+    // subscribe to the Observable and provide the code we would like to do with our data from the response
+    tempObservable.subscribe(data => console.log("Got our tasks!", data));
+  }
+
+  getTaskByID(){
+    // our http response is an Observable, store it in a variable
+    let tempObservable = this._http.get('/api/tasks/5c3f97a0d7140a2cc0bc8e65');
+    // subscribe to the Observable and provide the code we would like to do with our data from the response
+    tempObservable.subscribe(data => console.log("Got the task 5c3f97a0d7140a2cc0bc8e65", data));
+  }
+
+}
